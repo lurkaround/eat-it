6e8cab42ac0c32f71229946dbfc513427981a1a5
lurkaround <rhelon@gmail.com>
2021-04-27 09:40:00 +0200

    Added colors to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,18 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2ED5',
+          200: '#88883',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
